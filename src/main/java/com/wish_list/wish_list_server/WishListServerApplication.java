19c21b6859b1ac0214d1e2221288be11df4b968b
stevenquintana <steve.e.quint@gmail.com>
2024-12-19 09:43:41 -0800

    first commit

---
+++ src/main/java/com/wish_list/wish_list_server/WishListServerApplication.java
@@ -0,0 +1,13 @@
+package com.wish_list.wish_list_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wish_list/wish_list_server/WishListServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wish_list.wish_list_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
